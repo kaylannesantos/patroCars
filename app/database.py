e6b99b105dc0bbefe616c711c974e094c0624cfe
kaylannemendesdossantos@gmail.com <kaylannemendesdossantos@gmail.com>
2024-11-04 11:50:11 -0300

    mais uma pro front

--- app/database.py
+++ app/database.py
@@ -1,14 +1,11 @@
-import os
 import psycopg2
 
 def get_db_connection():
     banco = psycopg2.connect(
-        dbname= os.getenv("patrocars_jvgt"),
-        user= os.getenv("patrocars_jvgt_user"),
-        password= os.getenv("3SvpJJyK5PauzVt5UPhjFywPriLJUPPY"),
-        host= os.getenv("dpg-cskbfprtq21c73dlcqmg-a"),
-        port= os.getenv("5432")
+        dbname='patrocars',
+        user='postgres',
+        password='123',
+        host='localhost',
+        port='5432'
         )
-    #banco = psycopg2.connect(dbname='patrocars',user='postgres',password='123',host='localhost',port='5432')
-    #banco = psycopg2.connect("dbname='patrocars' user=postgres password='123' host=localhost port=5432")
     return banco
