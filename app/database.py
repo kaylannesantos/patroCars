13d36341ae752b49892dab7d9a1d1b907a977c92
Kaylanne Santos <102864770+kaylannesantos@users.noreply.github.com>
2024-11-04 21:17:12 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,14 @@
+#import psycopg2
+
+#def get_db_connection():
+    #banco = psycopg2.connect(dbname='patrocars',user='postgres',password='123',host='localhost',port='5432')
+    #banco = psycopg2.connect("dbname='patrocars' user=postgres password='123' host=localhost port=5432")
+
+    #return banco
+import os
 import psycopg2
 
 def get_db_connection():
-    #banco = psycopg2.connect(dbname='patrocars',user='postgres',password='123',host='localhost',port='5432')
-    banco = psycopg2.connect("dbname='patrocars' user=postgres password='123' host=localhost port=5432")
-
+    database_url = os.getenv("postgresql://userpostgres:hBK90sYkdDFXdFEYjsHfuGC2KQzuA3OF@dpg-cskm3jm8ii6s73ftdvtg-a/patrocars_oxal")
+    banco = psycopg2.connect(database_url)
     return banco
