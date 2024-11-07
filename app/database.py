3d3fcd680ec969da92e1505060ac14779c26e567
kaylannemendesdossantos@gmail.com <kaylannemendesdossantos@gmail.com>
2024-11-07 09:03:16 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,16 +1,15 @@
 import psycopg2
+import os
 
+def get_db_connection():
+    database_url = os.getenv("postgresql://userpostgre:lfxFlRe6L2bYmEA5jnwGBteTVX3M4vqy@dpg-csmamfjqf0us73fvbs10-a.ohio-postgres.render.com/patrocars_z8n0")
+    banco = psycopg2.connect(database_url)
+    return banco
+
+"""
 def get_db_connection():
     banco = psycopg2.connect(dbname='patrocars',user='postgres',password='123',host='localhost',port='5432')
     #banco = psycopg2.connect("dbname='patrocars' user=postgres password='123' host=localhost port=5432")
 
     return banco
-
 """
-import os
-def get_db_connection():
-    database_url = os.getenv("postgresql://userpostgres:hBK90sYkdDFXdFEYjsHfuGC2KQzuA3OF@dpg-cskm3jm8ii6s73ftdvtg-a.ohio-postgres.render.com/patrocars_oxal")
-    banco = psycopg2.connect(database_url)
-    return banco
-"""
-
