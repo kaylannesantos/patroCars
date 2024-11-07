e1466deaff9138efd6bb1e98c23787de7a944fa1
kaylannemendesdossantos@gmail.com <kaylannemendesdossantos@gmail.com>
2024-11-07 09:52:02 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,9 @@ import psycopg2
 import os
 
 def get_db_connection():
-    database_url = os.getenv("postgresql://userpostgre:lfxFlRe6L2bYmEA5jnwGBteTVX3M4vqy@dpg-csmamfjqf0us73fvbs10-a.ohio-postgres.render.com/patrocars_z8n0")
+    database_url = os.getenv("DATABASE_URL")  # Certifique-se de que a variável de ambiente está configurada como DATABASE_URL
+    if not database_url:
+        raise ValueError("DATABASE_URL environment variable is not set.")
     banco = psycopg2.connect(database_url)
     return banco
 
